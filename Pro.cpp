7c05b1c20cd929181b01694ee07720d58eb36ade
Thor Sideburns <sideboard@google.com>
2016-05-01 20:43:56 -0700

    yowza

--- Pro.cpp
+++ Pro.cpp
@@ -10,78 +10,93 @@ using namespace std;
 std::mutex g_out;
 
 std::mutex g_shutdown;
-std::condition_variable cv;
+std::condition_variable g_shutdown_cond;
 
-thread_local deque<string> m_messages;
+//thread_local deque<string> m_messages;
+
+class Job {
+public:
+  string name;
+};
+typedef std::shared_ptr<Job> JobPtr;
+  
 
 class Worker {
 private:
-
   thread m_executor;
+  deque<JobPtr> m_jobs;
 
-  void gen_message() {
-    string l{"LOGLINE"};
-    l += to_string(rand() % 100);
-    { 
+  std::mutex m_jobs_ready_mtx;
+  std::condition_variable m_jobs_ready_cond;
+
+  //void gen_message() {
+  //  string l{"LOGLINE"};
+  //  l += to_string(rand() % 100);
+  //  { 
+  //    lock_guard<mutex> guard(g_out);
+  //    cout << "[" << this_thread::get_id() << "] - Msg " << l << "\n";
+  //  }
+  //  m_messages.push_back(l);
+  //}
+
+  void run() {
+    {
       lock_guard<mutex> guard(g_out);
-      cout << "[" << this_thread::get_id() << "] - Msg " << l << "\n";
+      cout << "[" << this_thread::get_id() << "] Worker starting up yo!\n";
     }
-    m_messages.push_back(l);
+    while (true) {
+      while ( m_jobs.size() > 0 ) {
+          JobPtr j = m_jobs.front();
+          m_jobs.pop_front();
+          {
+            lock_guard<mutex> guard(g_out);
+            cout << "Popped a jobbie!\n";
+          }
+      }
+      std::unique_lock<std::mutex> lk(m_jobs_ready_mtx);
+      m_jobs_ready_cond.wait(lk);
+      {
+        lock_guard<mutex> guard(g_out);
+        cout << "Oh! new jobs ready!\n";
+      }
+    }
   }
 
 public:
-
   Worker() {
-    cout << "Worker starting up yo!\n";
+    m_executor = std::thread(&Worker::run, this);
   }
 
   ~Worker() {
     m_executor.join();
   }
 
-  void start() {
-    m_executor = std::thread(&Worker::run, this);
-  }
-
-
-  void run() {
-    while (true) {
-      gen_message();
+  void send(JobPtr j) {
+    {
+      lock_guard<mutex> guard(g_out);
+      cout << "received Job = type: " << j->name << "\n";
     }
+    m_jobs.push_back(j);
+    m_jobs_ready_cond.notify_all();
   }
 
-  void get_log() {
-    if ( !m_messages.empty() ) {
-      //{ 
-      //  lock_guard<mutex> guard(g_out);
-      //  cout << "[" << this_thread::get_id() << "] Before:: Size of messages: " << m_messages.size() << "\n";
-      //}
-      string l = m_messages.front();
-      m_messages.pop_front();
-      { 
-        lock_guard<mutex> guard(g_out);
-        cout << "[" << this_thread::get_id() << "] - Msg " << l << "\n";
-      }
-    }
-  }
 };
-
 typedef std::shared_ptr<Worker> WorkerPtr;
 
 class Logger {
+private:
+  vector<WorkerPtr> m_workers;
 public:
   void add_worker(WorkerPtr w) {
     m_workers.push_back(w);
   }
   void run() {
-    while (true) {
-      for ( auto w : m_workers )  {
-        w->get_log();
-      }
+    for ( auto w : m_workers )  {
+      JobPtr j = std::make_shared<Job>();
+      j->name = "blah";
+      w->send(j);
     }
   }
-private:
-  vector<WorkerPtr> m_workers;
 };
 
 int main()
@@ -89,19 +104,12 @@ int main()
   cout << "[" << this_thread::get_id() << "] Mainthread\n";
   srand (time(NULL));
   auto w1 = std::make_shared<Worker>();
-  w1->start();
-  auto w2 = std::make_shared<Worker>();
-  w2->start();
-  auto w3 = std::make_shared<Worker>();
-  w3->start();
 
   Logger l;
   l.add_worker(w1);
-  l.add_worker(w2);
-  l.add_worker(w3);
   l.run();
 
   std::unique_lock<std::mutex> lk(g_shutdown);
-  cv.wait(lk);
+  g_shutdown_cond.wait(lk);
   
 }
